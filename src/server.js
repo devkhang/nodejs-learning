ccafc6f64879cd52658d1ec58ef91a3437bf21a8
kevin khang <khangau98@gmail.com>
2024-07-20 13:44:45 +0700

    new update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
 const express = require('express');
 const ConfigViewEngine = require('./config/viewEngine')
 const webRouter = require('./router/web');
+const connection = require('./config/database');
 
 const app = express();
 const port = process.env.PORT||8080;
@@ -10,6 +10,16 @@ const host_name = process.env.HOST_NAME||'localhost';
 ConfigViewEngine(app);
 
 app.use(webRouter);
+
+
+connection.query(
+  'SELECT * FROM Users u',
+  function (err, results, fields) {
+    console.log("result>>>>="+JSON.stringify(results, null, 2)); // results contains rows returned by server
+    // console.log("fields>>>="+JSON.stringify(fields,null,2)); // fields contains extra meta data about results, if available
+  }
+);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
